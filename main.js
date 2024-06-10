0a299c71b16f525be3c5489d283101db5ef8053c
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-06-10 16:50:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let summerCollection = ["Bareeze lawn", 3, "SanaSafinaz lawn", 2, "Gul Ahmed lawn", 5, { name: "My collection" }];
+console.log(summerCollection);
+console.log(summerCollection[2]);
---
+++ main.ts
@@ -0,0 +1,7 @@
+let summerCollection: [string,number,string,number,string, number, {name:string}?] =
+["Bareeze lawn" , 3,"SanaSafinaz lawn", 2, "Gul Ahmed lawn", 5, {name: "My collection"}]
+console.log(summerCollection)
+console.log(summerCollection[2])
+
+
+
